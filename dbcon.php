22a2e9afb8d11184fb775ac914759fe601a70f06
somyaBIT <56736841+somyaBIT@users.noreply.github.com>
2021-03-16 17:29:34 +0530

    Add files via upload

--- dbcon.php
+++ dbcon.php
@@ -1,8 +1,8 @@
 <?php
-$server='localhost';
-$user='root';
-$password='';
-$db='corona';
+$server='sql213.epizy.com';
+$user='epiz_25923009';
+$password='lUBPmYr66M';
+$db='epiz_25923009_corona';
 
   $con=mysqli_connect($server,$user,$password,$db);
  
